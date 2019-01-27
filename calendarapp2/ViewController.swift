067df3f0e2d79641d3dd0127511d34ee5069adc0
Kan Nakamura <kan.nakamura0711@gmail.com>
2019-01-27 10:45:54 +0900

    Initial Commit

---
+++ calendarapp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calendarapp2
+//
+//  Created by Kan Nakamura on 2019/01/27.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
